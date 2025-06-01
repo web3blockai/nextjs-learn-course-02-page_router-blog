052be7d1948af4be5204310328bdc55c9545b64a
Alan T. <web3blockai@googlegroups.com>
2025-06-01 20:41:30 +0300

    text update

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>
+          Hello, I’m Alan. I’m a Next.js software engineer. You can contact me on{' '}
+          <a href="https://github.com/web3blockai">Github</a>.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
